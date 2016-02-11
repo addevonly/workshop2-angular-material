704e440cd299224d7087395750c4d28a0f33b782
Jordan Bisasky <jclounge222@gmail.com>
2016-02-11 12:09:14 -0500

    Completed step 1, adding step 2

--- www/appCtrl.js
+++ www/appCtrl.js
@@ -9,5 +9,19 @@
     var vm = this;
 
     vm.users = firebaseFactory.getAll('user');
+    vm.addNewUser = addNewUser;
+
+    /*
+      addNewUser
+      Method that calls firebaseFactory.insertDb to push a new user to the
+      Firebase instance
+      After insert call, clears the name and email
+      @param newUser: JSON object with the name and email
+    */
+    function addNewUser(newUser) {
+      firebaseFactory.insertDb('user', newUser);
+
+      // TODO: clear name and email
+    }
   }
 })();
