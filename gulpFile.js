2210bfcb20f1687c237f4702f4e8f84ab7ed98ba
Jordan Bisasky <jclounge222@gmail.com>
2016-04-15 20:18:15 -0400

    add minification

--- gulpFile.js
+++ gulpFile.js
@@ -1,10 +1,13 @@
 var gulp = require('gulp'),
     source = require('vinyl-source-stream'),
+    buffer = require('vinyl-buffer')
     browserify = require('browserify'),
     plumber = require('gulp-plumber'),
     watch = require('gulp-watch')
     nodemon = require('gulp-nodemon'),
-    livereload = require('gulp-livereload');
+    livereload = require('gulp-livereload'),
+    jshint = require('gulp-jshint'),
+    uglify = require('gulp-uglify');
 
 // Live Reload requires the Chrome extension Live Reload to also be downloaded
 // and activated
@@ -22,7 +25,16 @@ gulp.task('build', function() {
 // Watches the www folder, but ignores the www/dist.js
 gulp.task('watch', function() {
   livereload.listen();
-  gulp.watch(['www/*.js', 'www/*.html', '!www/dist.js'], ['build']);
+  gulp.watch(['www/*.js', 'www/*.html', '!www/dist.js'], ['build', 'lint']);
+});
+
+gulp.task('minify', function() {
+  return browserify({ entries: './www/app.js'})
+    .bundle()
+    .pipe(source('dist.min.js'))
+    .pipe(buffer())
+    .pipe(uglify())
+    .pipe(gulp.dest('./www/'))
 });
 
 // Starts the server and watches/restarts after changes
@@ -32,6 +44,12 @@ gulp.task('server', function() {
   });
 });
 
+gulp.task('lint', function() {
+  return gulp.src(['www/*.js', '!www/dist.js'])
+    .pipe(jshint())
+    .pipe(jshint.reporter('default'));
+})
+
 // Default script - type 'gulp' in the terminal to run
 // Builds the bundle, starts the server, and watches for changes
-gulp.task('default', ['build', 'server', 'watch']);
+gulp.task('default', ['build', 'lint', 'server', 'watch']);
